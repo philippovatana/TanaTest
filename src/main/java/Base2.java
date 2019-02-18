a0697671efca079dfb8f921cf5dcd07b3ded4c91
philippovatana <philippova_tana@mail.ru>
2019-02-18 09:55:06 +0300

    изменение репозитория

--- src/main/java/Base2.java
+++ src/main/java/Base2.java
@@ -1,2 +1,13 @@
+import java.util.Scanner;
+
 public class Base2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String day;
+        System.out.println("Какой сегодня день недели");
+        day = input.nextLine();
+        String text;
+        text = "Сегодня " + day;
+        System.out.println(text);
+    }
 }
